ebbdb339ef3e6f35ee48422cc7fb5c94dbc449c7
joseusuga <jose.usuga@ceiba.com.co>
2020-11-25 10:28:20 -0500

    AJUSTES: Inicializar el proyecto y agregar las dependencias de mongo, thymeleaf, dev tools, spring web reactive

---
+++ src/test/java/com/bolsadeideas/springboot/webflux/springwebflux/SpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.webflux.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
